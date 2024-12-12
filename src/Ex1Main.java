c9db4e14bede29243ed3c573507e2f1e6613c309
97254 <tamartuil1@gmail.com>
2024-12-12 23:50:27 +0200

    Done - Main

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -1,11 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * Intro2CS, Ex1 - very basic "main template"
- * Make sure your implementation of this main performs as the Ex1Sol.jar solution implement all needed functions.
- *
- */
+
 public class Ex1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,30 +12,30 @@ public class Ex1Main {
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
             num1 = sc.next();
             if (Ex1.isNumber(num1)) {
+                System.out.println("num1= " + num1 + " is number: true , value: " + Ex1.number2Int(num1));
             }
-            System.out.println("num1= " + num1 + " is number: true , value: ");
-            { else System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
+            else {
+                System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
             }
-            if (Ex1.number2Int(num1)) {
+            if ((!num1.equals("quit")) && (num1.isEmpty())) {
+                break;
             }
-            if (!num1.equals("quit")) {
-            }
-            break;
+            else continue;
         }
 
         while (!num1.equals("quit") && !num2.equals("quit")) {
             System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
             num2 = sc.next();
             if (Ex1.isNumber(num2)) {
+                System.out.println("num2= " + num2 + " is number: true , value: " + Ex1.number2Int(num2));
             }
-            { else System.out.println("ERR: num2 is in the wrong format! (" + num2 + ")");
+             else {
+                 System.out.println("ERR: num2 is in the wrong format! (" + num2 + ")");
             }
-            System.out.println("num2= " + num2 + "is number: true , value: ");
-            if (Ex1.number2Int(num2)) {
+            if ((!num2.equals("quit")) && (num2.isEmpty())) {
+                break;
             }
-            if (!num2.equals("quit")) {
-            }
-            break;
+            else continue;
         }
 
         System.out.println("Enter a base for output: (a number [2,16])");
@@ -49,7 +45,7 @@ public class Ex1Main {
         String[] arr = {num1, num2, valueSumPLUS, valueSumMULTI};
         int maxIndex = Ex1.maxIndex(arr);
         String maxNumber = arr[maxIndex];
-        if (Ex1.isValidBasePart(base)) {
+        if (Ex1.isValidBasePart(String.valueOf(base))) {
             System.out.println(num1 + " + " + num2 + " = " + valueSumPLUS);
             System.out.println(num1 + " * " + num2 + " = " + valueSumMULTI);
             System.out.println("Max number over " + Arrays.toString(arr)+ "is: " + maxNumber);
