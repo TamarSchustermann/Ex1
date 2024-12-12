f4e07fff90652dcb21e3cb1bc559b21d016b5c35
97254 <tamartuil1@gmail.com>
2024-12-12 23:07:55 +0200

    before recent repairs - Main

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,48 +10,51 @@ public class Ex1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String num1 = "", num2 = "", quit = "quit";
-        while (!num1.equals(quit) && !num2.equals(quit)) {
+        while (!num1.equals("quit") && !num2.equals("quit")) {
             System.out.println();
             System.out.println("Ex1 class solution:");
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
             num1 = sc.next();
-            if (!num1.equals("quit")) {
-            }
             if (Ex1.isNumber(num1)) {
+            }
+            System.out.println("num1= " + num1 + " is number: true , value: ");
+            { else System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
             }
             if (Ex1.number2Int(num1)) {
             }
-            System.out.println(num1);
-            { else System.out.println("num1= " + num1 + " is number: false , value: -1");
-                System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
+            if (!num1.equals("quit")) {
             }
             break;
         }
 
-        while (!num2.equals(quit) && !num2.equals(quit)) {
+        while (!num1.equals("quit") && !num2.equals("quit")) {
             System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
             num2 = sc.next();
-            if (!num2.equals("quit")) {
-            }
             if (Ex1.isNumber(num2)) {
             }
+            { else System.out.println("ERR: num2 is in the wrong format! (" + num2 + ")");
+            }
+            System.out.println("num2= " + num2 + "is number: true , value: ");
             if (Ex1.number2Int(num2)) {
             }
-            System.out.println(num2);
-            else System.out.println("num2= " + num2 + " is number: false , value: -1");
-                System.out.println("ERR: num2 is in the wrong format! (" + num2 + ")");
+            if (!num2.equals("quit")) {
+            }
             break;
         }
 
         System.out.println("Enter a base for output: (a number [2,16])");
-        int base ;
+        int base = Integer.parseInt(sc.next());
+        String valueSumPLUS = Ex1.int2Number(Ex1.number2Int(num1) + Ex1.number2Int(num2), base);
+        String valueSumMULTI = Ex1.int2Number(Ex1.number2Int(num1) * Ex1.number2Int(num2), base);
+        String[] arr = {num1, num2, valueSumPLUS, valueSumMULTI};
+        int maxIndex = Ex1.maxIndex(arr);
+        String maxNumber = arr[maxIndex];
         if (Ex1.isValidBasePart(base)) {
-            System.out.println(num1 + " + " + num2 + " = " + );
-            System.out.println(num1 + " * " + num2 + " = " + );
-            System.out.println("Max number over " + + "is: " + );
+            System.out.println(num1 + " + " + num2 + " = " + valueSumPLUS);
+            System.out.println(num1 + " * " + num2 + " = " + valueSumMULTI);
+            System.out.println("Max number over " + Arrays.toString(arr)+ "is: " + maxNumber);
         }
         else System.out.println("ERR: wrong base, should be [2,16], got (" + base + ")");
-
         System.out.println("quiting now...");
     }
 }
