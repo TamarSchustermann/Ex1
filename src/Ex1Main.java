9ab87c043961374a3c9a5704024e4674a7ff5dc5
97254 <tamartuil1@gmail.com>
2024-12-12 11:14:12 +0200

    Start Main

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Ex1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String num1 = "", num2="", quit = "quit";
+        String num1 = "", num2 = "", quit = "quit";
         while (!num1.equals(quit) && !num2.equals(quit)) {
             System.out.println();
             System.out.println("Ex1 class solution:");
@@ -16,19 +16,40 @@ public class Ex1Main {
             num1 = sc.next();
             if (!num1.equals("quit")) {
             }
-            if (isNumber(num1)){
-                if (number2Int)
+            if (isNumber(num1)) {
             }
-            else System.out.print("num1= " + num1 + " is number: false , value: -1");
-                 System.out.print("ERR: num1 is in the wrong format! ("+num1+")");
+            if (number2Int(num1)) {
+            }
+            System.out.print(num1);
+            { else System.out.print("num1= " + num1 + " is number: false , value: -1");
+                System.out.print("ERR: num1 is in the wrong format! (" + num1 + ")");
+            }
             break;
-
-
-                // add your code here
-
-                /////////////////////
-            }
         }
+
+        while (!num2.equals(quit) && !num2.equals(quit)) {
+            System.out.println();
+            System.out.println("Ex1 class solution:");
+            System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
+            num2 = sc.next();
+            if (!num2.equals("quit")) {
+            }
+            if (isNumber(num2)) {
+            }
+            if (number2Int(num2)) {
+            }
+            System.out.print(num2);
+            else System.out.print("num2= " + num2 + " is number: false , value: -1");
+                System.out.print("ERR: num2 is in the wrong format! (" + num2 + ")");
+
+            break;
+        }
+        String base = "";
+        System.out.print("תכניס בסיס שבאלך");
+        if (isValidBasePart(base)) {
+        }
+
+        else System.out.print("מה שכתוב בפלט של בועז כשמכניסים בסיס לא תקין");
         System.out.println("quiting now...");
     }
 }
